df1758961e4ea6a2547fe5b782946c0fe696ac1e
Pravin Rathod <pravinrathod206@gmail.com>
2023-12-27 18:25:56 +0400

    connected front end to back end

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,23 +1,57 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import articles from './article-content';
-import NotFoundPage from './NotFoundPage'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
+import axios from "axios";
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
-    const articleId = useParams().article
-    console.log(articleId);
-    const article = articles.find(article => article.name === articleId)
+    const articleId = useParams().article;
+
+    const [articlesInfo, setArticlesInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticlesInfo(newArticleInfo);
+        };
+        loadArticleInfo();
+    }, []);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticleInfo = response.data;
+        setArticlesInfo(updatedArticleInfo);
+    };
+
+    const article = articles.find((article) => article.name === articleId);
     if (!article) {
-        return <NotFoundPage />
+        return <NotFoundPage />;
     }
     return (
         <>
             <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This article has {articlesInfo.upvotes} upvote(s) </p>
+            </div>
             {article.content.map((p, i) => (
                 <p key={i}>{p}</p>
             ))}
+            <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={(updatedArticle) =>
+                    setArticlesInfo(updatedArticle)
+                }
+            />
+            <CommentsList comments={articlesInfo.comments} />
         </>
-    )
-}
+    );
+};
 
-export default ArticlePage
+export default ArticlePage;
