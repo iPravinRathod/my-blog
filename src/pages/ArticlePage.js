6c95da9cc83c56b8ebfe553d29a9adbe9dfc575a
Pravin Rathod <pravinrathod206@gmail.com>
2024-01-23 18:14:16 +0400

    created custom auth hook (useUser)

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,15 +5,16 @@ import NotFoundPage from "./NotFoundPage";
 import axios from "axios";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
+import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
-    const articleId = useParams().article;
-
     const [articlesInfo, setArticlesInfo] = useState({
         upvotes: 0,
         comments: [],
     });
-
+    const articleId = useParams().article;
+    const { user, isLoading } = useUser();
+    console.log("user" + JSON.stringify(user));
     useEffect(() => {
         const loadArticleInfo = async () => {
             const response = await axios.get(`/api/articles/${articleId}`);
@@ -37,18 +38,31 @@ const ArticlePage = () => {
         <>
             <h1>{article.title}</h1>
             <div className="upvotes-section">
-                <button onClick={addUpvote}>Upvote</button>
+                {user ? (
+                    <button onClick={addUpvote}>Upvote</button>
+                ) : (
+                    <button>Log In to upvote</button>
+                )}
                 <p>This article has {articlesInfo.upvotes} upvote(s) </p>
             </div>
             {article.content.map((p, i) => (
                 <p key={i}>{p}</p>
             ))}
-            <AddCommentForm
-                articleName={articleId}
-                onArticleUpdated={(updatedArticle) =>
-                    setArticlesInfo(updatedArticle)
-                }
-            />
+            {user ? (
+                <AddCommentForm
+                    articleName={articleId}
+                    onArticleUpdated={(updatedArticle) =>
+                        setArticlesInfo(updatedArticle)
+                    }
+                />
+            ) : (
+                <AddCommentForm
+                    articleName={articleId}
+                    onArticleUpdated={(updatedArticle) =>
+                        setArticlesInfo(updatedArticle)
+                    }
+                />
+            )}
             <CommentsList comments={articlesInfo.comments} />
         </>
     );
