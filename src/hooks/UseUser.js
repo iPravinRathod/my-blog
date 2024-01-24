848a31340858abed39d14d6b9a4f1d1992b0ee8e
Pravin Rathod <pravinrathod206@gmail.com>
2024-01-24 18:03:44 +0400

    changed is loading to true

--- src/hooks/UseUser.js
+++ src/hooks/UseUser.js
@@ -2,7 +2,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 const useUser = () => {
     const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
