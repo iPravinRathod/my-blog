e24e71e57d5a119be4d32ffa7377856be3a1ff4b
Pravin Rathod <pravinrathod206@gmail.com>
2024-01-24 22:26:22 +0400

    set up hosting for mongodb

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import articles from "./article-content";
-import NotFoundPage from "./NotFoundPage";
 import axios from "axios";
+import NotFoundPage from "./NotFoundPage";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
 import useUser from "../hooks/useUser";
+import articles from "./article-content";
 
 const ArticlePage = () => {
-    const [articlesInfo, setArticlesInfo] = useState({
+    const [articleInfo, setArticleInfo] = useState({
         upvotes: 0,
         comments: [],
         canUpvote: false,
     });
-    const { canUpvote } = articlesInfo;
+    const { canUpvote } = articleInfo;
     const articleId = useParams().article;
+
     const { user, isLoading } = useUser();
 
     useEffect(() => {
@@ -25,28 +26,32 @@ const ArticlePage = () => {
                 headers,
             });
             const newArticleInfo = response.data;
-            setArticlesInfo(newArticleInfo);
+            setArticleInfo(newArticleInfo);
         };
 
-        if (isLoading) {
+        if (!isLoading) {
             loadArticleInfo();
         }
     }, [isLoading, user]);
 
+    const article = articles.find((article) => article.name === articleId);
+
     const addUpvote = async () => {
         const token = user && (await user.getIdToken());
         const headers = token ? { authtoken: token } : {};
-        const response = await axios.put(`/api/articles/${articleId}/upvote`, {
-            headers,
-        });
-        const updatedArticleInfo = response.data;
-        setArticlesInfo(updatedArticleInfo);
+        const response = await axios.put(
+            `/api/articles/${articleId}/upvote`,
+            null,
+            { headers }
+        );
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
     };
 
-    const article = articles.find((article) => article.name === articleId);
     if (!article) {
         return <NotFoundPage />;
     }
+
     return (
         <>
             <h1>{article.title}</h1>
@@ -56,24 +61,24 @@ const ArticlePage = () => {
                         {canUpvote ? "Upvote" : "Already Upvoted"}
                     </button>
                 ) : (
-                    <button>Log In to upvote</button>
+                    <button>Log in to upvote</button>
                 )}
-                <p>This article has {articlesInfo.upvotes} upvote(s) </p>
+                <p>This article has {articleInfo.upvotes} upvote(s)</p>
             </div>
-            {article.content.map((p, i) => (
-                <p key={i}>{p}</p>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
             ))}
             {user ? (
                 <AddCommentForm
                     articleName={articleId}
                     onArticleUpdated={(updatedArticle) =>
-                        setArticlesInfo(updatedArticle)
+                        setArticleInfo(updatedArticle)
                     }
                 />
             ) : (
                 <button>Log in to add a comment</button>
             )}
-            <CommentsList comments={articlesInfo.comments} />
+            <CommentsList comments={articleInfo.comments} />
         </>
     );
 };
